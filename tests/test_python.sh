6d094748f6f60c14631bfbe38ec26c1b438e7a1b
johnfan <johndoe@example.com>
2023-01-12 16:34:36 +0100

    Fix CI tests.

--- tests/test_python.sh
+++ tests/test_python.sh
@@ -2,10 +2,4 @@
 set -ex
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# pylint --rcfile ${DIR}/../python/legion_linux/pylintrc ${DIR}/../python/legion_linux
-
-
-# # Smoketest CLI
-# ${DIR}/../python/legion_linux/legion_cli.py
-
-xvfb-run "${DIR}/../python/legion_linux/legion_gui.py"
+pylint --rcfile ${DIR}/../python/legion_linux/pylintrc ${DIR}/../python/legion_linux
--- tests/test_python_cli.sh
+++ tests/test_python_cli.sh
@@ -2,5 +2,8 @@
 set -ex
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Mock HWMON for tet
+mkdir -p /sys/module/legion_laptop/drivers/platform:legion/PNP0C09:00/hwmon/hwmon0
+
 # Smoketest CLI
 ${DIR}/../python/legion_linux/legion_cli.py
