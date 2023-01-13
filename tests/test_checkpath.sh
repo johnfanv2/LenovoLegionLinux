f5d97b3fd2ac72c3eb87b3c8fad532efd34ddf31
johnfan <johndoe@example.com>
2023-01-13 23:29:46 +0100

    Use absolute dirs for checkpath test.

--- tests/test_checkpath.sh
+++ tests/test_checkpath.sh
@@ -2,4 +2,4 @@
 set -ex
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-${DIR}/../deploy/dependencies/install_dir/checkpatch.pl -f --no-tree kernel_module/legion-laptop.c
+${DIR}/../deploy/dependencies/install_dir/checkpatch.pl -f --no-tree ${DIR}/../kernel_module/legion-laptop.c
