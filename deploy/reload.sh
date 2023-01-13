d95f089794e4c4a2c81372e9d76b2c898190a1c0
johnfan <johndoe@example.com>
2023-01-13 23:29:25 +0100

    Add reloading to CI tests.

--- deploy/reload.sh
+++ deploy/reload.sh
@@ -2,5 +2,9 @@
 set -ex
 cd kernel_module
 make
+
+find /lib/modules/$(uname -r) -type f -name '*.ko'
+sudo modprobe wmi || true 
+sudo modprobe platform_profile || true
 sudo make reloadmodule || true
 sudo dmesg
