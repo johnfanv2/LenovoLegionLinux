48c465f19fe575006ce02e64be8eff95d20e9a5a
johnfan <johndoe@example.com>
2023-01-31 20:13:43 +0100

    Update python install script.

--- deploy/build_python.sh
+++ deploy/build_python.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -ex
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${DIR}/../
+
 cd python/legion_linux
 python3 -m pip install --upgrade build
 python3 -m build
 sudo python3 -m pip install -e .
 
-# # Desktop file
-# sudo cp legion_gui.desktop /usr/share/applications/
-# sudo mkdir -p /usr/share/icons/
-# sudo cp legion_logo.png /usr/share/icons/legion_logo.png
+# Desktop file
+sudo cp legion_gui.desktop /usr/share/applications/
+sudo mkdir -p /usr/share/icons/
+sudo cp legion_logo.png /usr/share/icons/legion_logo.png
 
