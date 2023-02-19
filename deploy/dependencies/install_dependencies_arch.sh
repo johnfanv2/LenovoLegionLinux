3e891105776882a4d9d0a7d0f17c67e0380eb280
johnfan <johndoe@example.com>
2023-02-19 03:37:20 +0100

    Start GUI from dekstop with pkexec.

--- deploy/dependencies/install_dependencies_arch.sh
+++ deploy/dependencies/install_dependencies_arch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ex
-sudo pacman -S --noconfirm linux-headers base-devel lm_sensors git dmidecode python-pyqt5 python-yaml python-argcomplete
+sudo pacman -S --noconfirm linux-headers base-devel lm_sensors git dmidecode python-pyqt5 python-yaml python-argcomplete polkit
