5f9498a2a17214bb453f76696d3388a9483e76d5
ARYN <53664593+ARYN-27@users.noreply.github.com>
2023-04-02 07:58:55 +0800

    Update patch_kernel.sh

--- kernel_module/patch_kernel.sh
+++ kernel_module/patch_kernel.sh
@@ -5,25 +5,39 @@
 
 kernel_patching() {
 
-    local privup
-    local catit
+ 	LC_ALL=C
+	LANG=C
 
-    privup=""
-    catit="cat"
-    # checks if is root to not use sudo or doas
-    if [[ ! "${USER}" ]]; then
-    # checks if doas is installed and use it as default
-        [[ -f /usr/bin/doas ]] && privup="doas" || privup="sudo"
-    fi
-    # check if bat is installed and use it as default
-    [[ -f /usr/bin/bat ]] && catit="/usr/bin/bat --paging never" || catit="/usr/bin/cat"
+	local krn_dir
+	local privup
+	local catit
+	local -i reload_verbosity
 
-    make
-    "${privup}" make install
-    "${privup}" make reloadmodule | tail --lines=5 | ${catit}
-    "${privup}" systemctl restart power-profiles-daemon --no-pager --show-transaction
+	krn_dir="kernel_module"
+	catit="cat"
+	# checks if is root to not use sudo or doas
+	if [[ ! "${USER}" = 1000 ]]; then
+		# checks if doas is installed and use it as default
+		[[ -f /usr/bin/doas ]] && privup="doas" || privup="sudo"
+	fi
+	# check if bat is installed and use it as default
+	[[ -f /usr/bin/bat ]] && catit="/usr/bin/bat --paging never" || catit="/usr/bin/cat"
+	# reload_verbosity
+	# 1 = just the last message if it's loaded or nor
+	# 5 = last five lines including success
+	# 41 = all latest messages related to unloading and loading the LLL module
+	reload_verbosity=1
 
-    printf "%s %s\n" "Patching done." "$?"
+	printf "Starting patching process for kernel-%s\n\n" "$(uname -r)"
+
+	pushd "${krn_dir}" || return
+	make
+	${privup} make install
+	${privup} make reloadmodule | tail --lines="${reload_verbosity}" | ${catit}
+	${privup} systemctl restart power-profiles-daemon --no-pager --show-transaction
+	popd || return
+	
+	printf "\n%s %s\n" "Patching process done. Exit %s" "$?"
 
 }
 
