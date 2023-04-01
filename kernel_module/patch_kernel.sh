db9d8c6432419063ab56109310bf5a39b1e7fc37
ARYN <53664593+ARYN-27@users.noreply.github.com>
2023-04-01 21:58:14 +0800

    Update patch_kernel.sh

--- kernel_module/patch_kernel.sh
+++ kernel_module/patch_kernel.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-make
-sudo make install
-sudo make reloadmodule | tail -n 5 | bat
-sudo systemctl restart power-profiles-daemon
+# Checks for doas, sudo, bat and cat first
+# Runs make and restarts power-profiles-daemon
 
-echo "Done"
+kernel_patching() {
+
+    local privup
+    local catit
+
+    privup=""
+    catit="cat"
+    # checks if is root to not use sudo or doas
+    if [[ ! "${USER}" ]]; then
+    # checks if doas is installed and use it as default
+        [[ -f /usr/bin/doas ]] && privup="doas" || privup="sudo"
+    fi
+    # check if bat is installed and use it as default
+    [[ -f /usr/bin/bat ]] && catit="/usr/bin/bat --paging never" || catit="/usr/bin/cat"
+
+    make
+    "${privup}" make install
+    "${privup}" make reloadmodule | tail --lines=5 | ${catit}
+    "${privup}" systemctl restart power-profiles-daemon --no-pager --show-transaction
+
+    printf "%s %s\n" "Patching done." "$?"
+
+}
+
+kernel_patching
