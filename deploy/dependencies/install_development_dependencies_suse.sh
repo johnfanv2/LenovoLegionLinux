61a4561332f713831fc44e24dffbd3921d7cbbc2
johnfan <johndoe@example.com>
2023-04-02 17:44:34 +0200

    Fix suse container for testing.

--- deploy/dependencies/install_development_dependencies_suse.sh
+++ deploy/dependencies/install_development_dependencies_suse.sh
@@ -8,7 +8,7 @@ sudo zypper refresh
 # Linter
 # Tools for running GUI tests headless
 # install directly from package file because which dependency broken
-sudo rpm -i --nodeps https://download.opensuse.org/repositories/openSUSE:/Factory/standard/noarch/xvfb-run-1.5.2-7.1.noarch.rpm
+sudo rpm -i --nodeps https://download.opensuse.org/repositories/openSUSE:/Factory/standard/noarch/xvfb-run-1.5.2-7.2.noarch.rpm
 # sudo zypper -n install --force --best-effort xvfb-run
 sudo zypper --non-interactive install \
     wget \
