c40393b9fce6137dc20e0287851d5550a614e3e0
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-04-05 20:43:06 +0100

    update dkms.conf and load module after build

--- deploy/build-dkms.sh
+++ deploy/build-dkms.sh
@@ -8,13 +8,19 @@ else
     cd kernel_module
 
     #Copy folder for dkms
-    if [ -d "/usr/src/LenovoLegionLinux-9999" ]; then
-        rm -r /usr/src/LenovoLegionLinux-9999
+    if [ -d "/usr/src/LenovoLegionLinux-1.0.0" ]; then
+        dkms remove -m LenovoLegionLinux -v 1.0.0
+        rm -r /usr/src/LenovoLegionLinux-1.0.0
     fi
-    mkdir /usr/src/LenovoLegionLinux-9999
-    cp -r * /usr/src/LenovoLegionLinux-9999
+    mkdir /usr/src/LenovoLegionLinux-1.0.0
+    cp -r * /usr/src/LenovoLegionLinux-1.0.0
 
     #Build and install as dkms module
-    dkms build LenovoLegionLinux --force -v 9999
-    dkms install LenovoLegionLinux --force -v 9999
+    dkms add -m LenovoLegionLinux -v 1.0.0
+    dkms build LenovoLegionLinux --force -v 1.0.0
+    dkms install LenovoLegionLinux --force -v 1.0.0
+
+    #load the module
+    modprobe legion_laptop
+    dmesg | grep legion_laptop
 fi
