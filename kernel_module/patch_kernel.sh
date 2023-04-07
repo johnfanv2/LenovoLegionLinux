cb86cb461ada355eb62a0b0df7191ff798afa08b
Marcelo dos Santos Mafra <msmafra@gmail.com>
2023-04-07 13:10:29 -0300

    Improved patch script based on @ARYN-27 version. Added parameters build, load and install for just build, testing on current session or permanent patching current kernel. Plus 'verbosity level' with number of lines to show after done: 1, 5 or 41

--- kernel_module/patch_kernel.sh
+++ kernel_module/patch_kernel.sh
@@ -1,44 +1,75 @@
 #!/usr/bin/env bash
-
+# From the discussion https://github.com/johnfanv2/LenovoLegionLinux/pull/29
+##########################################
 # Checks for doas, sudo, bat and cat first
-# Runs make and restarts power-profiles-daemon
+# Runs make, make reloadmodule and make install.
+# Also reloads/restarts power-profiles-daemon when done.
+# Test mode and Install mode
+##########################################
+lll_kernel_patching()
+                      {
+    LC_ALL=C
+    LANG=C
 
-kernel_patching() {
+    local script_folder
+    local kernel_version
+    local privup
+    local catit
+    local -i reload_verbosity
+    local lll_mode
+    local usage_message
 
- 	LC_ALL=C
-	LANG=C
+    script_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    kernel_version="$(uname -r)"
+    catit="cat"
+    # checks if is root to not use sudo or doas
+    if [[ ! "${USER}" = 1000 ]]; then
+        # checks if doas is installed and use it as default
+        [[ -f /usr/bin/doas ]] && privup="doas" || privup="sudo"
+    fi
+    # check if bat is installed and use it as default
+    [[ -f /usr/bin/bat ]] && catit="/usr/bin/bat --paging never" || catit="/usr/bin/cat"
+    # reload_verbosity (number of lines)
+    # 1 = just the last message if it's loaded or nor (default)
+    # 5 = last five lines including success (@ARYN-27 default)
+    # 41 = all latest messages related to unloading and loading the LLL module
+    [[ "${2}" =~ 1|5|41 ]] && reload_verbosity="${2}" || reload_verbosity=1
+    lll_mode="build|load|install"
 
-	local krn_dir
-	local privup
-	local catit
-	local -i reload_verbosity
+    if [[ $# -gt 0 ]] && [[ $# -lt 3 ]]; then
 
-	krn_dir="kernel_module"
-	catit="cat"
-	# checks if is root to not use sudo or doas
-	if [[ ! "${USER}" = 1000 ]]; then
-		# checks if doas is installed and use it as default
-		[[ -f /usr/bin/doas ]] && privup="doas" || privup="sudo"
-	fi
-	# check if bat is installed and use it as default
-	[[ -f /usr/bin/bat ]] && catit="/usr/bin/bat --paging never" || catit="/usr/bin/cat"
-	# reload_verbosity
-	# 1 = just the last message if it's loaded or nor
-	# 5 = last five lines including success
-	# 41 = all latest messages related to unloading and loading the LLL module
-	reload_verbosity=1
+        if [[ "${1}" =~ ${lll_mode[*]} ]]; then
 
-	printf "Starting patching process for kernel-%s\n\n" "$(uname -r)"
+            pushd "${script_folder}" || return
+            printf "Running %s mode.\n" "${1}"
+            printf "Starting patching process for kernel-%s\n\n" "${kernel_version}"
+            make
+            [[ "${1}" = "build" ]] && exit 0
+            if [[ "${1}" = "install" ]]; then
+                echo MAKEINSTALL
+                ${privup} make install
+            fi
+            ${privup} make reloadmodule | tail --lines="${reload_verbosity}" | ${catit}
+            ${privup} systemctl restart power-profiles-daemon --no-pager --show-transaction
+            printf "Patching process done. Exit %s\n" "$?"
+            popd || return
 
-	pushd "${krn_dir}" || return
-	make
-	${privup} make install
-	${privup} make reloadmodule | tail --lines="${reload_verbosity}" | ${catit}
-	${privup} systemctl restart power-profiles-daemon --no-pager --show-transaction
-	popd || return
-	
-	printf "\n%s %s\n" "Patching process done. Exit %s" "$?"
+        fi
+
+    else
+
+        usage_message="
+        Accepted parameters: ${lll_mode[*]} + 1|5|41
+        build = Just builds the module
+        install (permanent) = Permanently install the module for the current kernel
+        load (test mode) = Applies the patch for the current session.
+        And the values for verbosity:
+        1 = just the last message if it's loaded or nor (default)
+        5 = last five lines including success (@ARYN-27 default)
+        41 = all latest messages related to unloading and loading the LLL module"
+        printf "%s" "${usage_message}"
+
+    fi
 
 }
-
-kernel_patching
+lll_kernel_patching "${@}"
