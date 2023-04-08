6b5c1cd05af688b464743a8af8a3af0e7cbbb73c
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-04-08 11:45:06 +0100

    update build-dkms and makefile

--- deploy/build-dkms.sh
+++ deploy/build-dkms.sh
@@ -9,15 +9,16 @@ cd ..
 cd kernel_module
 
 #Copy folder for dkms
-if [ -d "/usr/src/LenovoLegionLinux-1.0.0" ]; then
-    dkms remove -m LenovoLegionLinux -v 1.0.0
-    rm -r /usr/src/LenovoLegionLinux-1.0.0
+if [ -d /usr/src/LenovoLegionLinux-1.0.0 ]; then
+	rm -r /usr/src/LenovoLegionLinux-1.0.0/*
+	cp -r * /usr/src/LenovoLegionLinux-1.0.0/
+else
+    mkdir /usr/src/LenovoLegionLinux-1.0.0
+    cp -r * /usr/src/LenovoLegionLinux-1.0.0
+    dkms add -m LenovoLegionLinux -v 1.0.0
 fi
-mkdir /usr/src/LenovoLegionLinux-1.0.0
-cp -r * /usr/src/LenovoLegionLinux-1.0.0
 
 #Build and install as dkms module
-dkms add -m LenovoLegionLinux -v 1.0.0
 dkms build LenovoLegionLinux --force -v 1.0.0
 dkms install LenovoLegionLinux --force -v 1.0.0
 
