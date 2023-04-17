0c680a577967b3cc1c55373f225ebd2f92e7fd2a
johnfan <johndoe@example.com>
2023-04-17 03:59:22 +0200

    Disable fan speed access for idepad h3cn.

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -425,7 +425,10 @@ static const struct model_config model_h3cn = {
 	.access_method_powermode = ACCESS_METHOD_WMI,
 	// not implemented (properly) in WMI, RGB conrolled by USB
 	.access_method_keyboard = ACCESS_METHOD_NO_ACCESS,
-	.access_method_fanspeed = ACCESS_METHOD_WMI,
+	// accessing fan speed is not implemented in ACPI 
+	// a variable in the operation region (or not found)
+	// and not per WMI (methods returns constant 0)
+	.access_method_fanspeed = ACCESS_METHOD_NO_ACCESS,
 	.access_method_temperature = ACCESS_METHOD_WMI,
 	.access_method_fancurve = ACCESS_METHOD_NO_ACCESS,
 	.acpi_check_dev = false
