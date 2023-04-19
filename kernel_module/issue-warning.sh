9e14c6f1cccd6897681300d41e329a4e7ed23035
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-04-19 11:33:53 +0100

    add warning for 2022/2023 issue

---
+++ kernel_module/issue-warning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+BOLD=$(tput bold)
+
+echo -e "${RED} ${BOLD} If you have a 2022 or 2023 model, please help testing the new features '\e]8;;https://github.com/johnfanv2/LenovoLegionLinux/issues/46\ahere\e]8;;\a'"
