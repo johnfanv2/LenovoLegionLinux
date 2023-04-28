44e439a3b527aa778324fde3159ce76ad83e99d0
johnfan <johndoe@example.com>
2023-04-28 23:14:35 +0200

    Fix arch test install script.

--- deploy/test_build_arch_python.sh
+++ deploy/test_build_arch_python.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd python/legion_linux
 python3 -m build --wheel --no-isolation
-python -m installer dist/*.whl
-python3 -m installer --destdir="/abc" dist/*.whl
+python3 -m installer --destdir="/" dist/*.whl
