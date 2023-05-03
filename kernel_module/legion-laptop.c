3cb2373fdf00f9236c10ea39337de1e26644d2c3
johnfan <johndoe@example.com>
2023-05-03 13:04:29 +0200

    Fix adress of EC im memory for E9CN

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -470,7 +470,7 @@ static const struct model_config model_e9cn = {
 	.access_method_temperature = ACCESS_METHOD_WMI,
 	.access_method_fancurve = ACCESS_METHOD_NO_ACCESS,
 	.acpi_check_dev = false,
-	.ramio_physical_start = 0xFE0B0800,
+	.ramio_physical_start = 0xFC7E0800,
 	.ramio_size = 0x600
 };
 
