0a6cb6ef8b7da6a65d83cd4edcda3cf5ef1f8d3e
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-05-29 16:50:29 +0100

    fix setup.cfg

--- deploy/python_install_installer_pkg.sh
+++ deploy/python_install_installer_pkg.sh
@@ -6,4 +6,7 @@ cd ${DIR}/../python/legion_linux
 # Build and install python package with build and installer
 python3 -m build --wheel --no-isolation
 python3 -m installer --destdir="/" dist/*.whl
+
+sudo cp legion_linux/legion_gui.desktop /usr/share/applications/
+
 echo "Done"
