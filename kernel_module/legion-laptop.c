1397222c1f15af717fb948c0453ce7f2009f8328
johnfan <johndoe@example.com>
2023-06-11 21:43:09 +0200

    Add bios model KWCN to kernel model

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -643,6 +643,15 @@ static const struct dmi_system_id optimistic_allowlist[] = {
 		},
 		.driver_data = (void *)&model_8jcn
 	},
+	{
+		// e.g. Legion 2023 
+		.ident = "8JCN",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_BIOS_VERSION, "KWCN"),
+		},
+		.driver_data = (void *)&model_v0
+	},
 	{}
 };
 
