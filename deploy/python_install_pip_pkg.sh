69949bdf0c93eb29bd54d3191c3bd6f699482415
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-07-28 20:47:39 +0100

    update scripts and desktop file  * fix python install on ubuntu  * fix workflow

--- deploy/python_install_pip_pkg.sh
+++ deploy/python_install_pip_pkg.sh
@@ -4,16 +4,17 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${DIR}/../python/legion_linux
 
 # Build and install python package
-python3 -m pip install --upgrade build
+python3 -m pip install --upgrade build installer
 python3 -m build
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root to install"
   exit
 else
-    python3 -m pip install -e .
+    python3 -m installer --destdir="/" dist/*.whl
     #Create config folder
     cp -r /usr/share/legion_linux /etc/legion_linux
+    cp -r /usr/local/share/legion_linux /etc/legion_linux
 fi
 
 echo "Done"
--- extra/systemd_install.sh
+++ extra/systemd_install.sh
@@ -8,19 +8,10 @@ fi
 
 read -p "This script Install systemd service" -n 1 -r
 
-cp service/fancurve-set /usr/bin/
-
-mkdir /usr/share/legion_linux/
-
-cp service/.env /etc/lenovo_linux/
-cp service/.env /usr/share/legion_linux/
-
-cp -r service/profiles/* /etc/lenovo_linux
-cp -r service/profiles/* /usr/share/legion_linux/
-
 sudo cp service/{legion-linux.service,legion-linux.path} /etc/systemd/system/
 
 #force to disable the service (pls enable in the gui)
+systemctl daemon-reload
 systemctl disable --now legion-linux.service,legion-linux.path
 
 echo "Done"
