e78cb286e34b65d066d789717ef89ef366a559e0
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-07-31 20:10:16 +0100

    revert commit 0d0551a  * @johnfanv2 need to find a better alternative

--- deploy/python_uninstall_pkg.sh
+++ deploy/python_uninstall_pkg.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -ex
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd ${DIR}/../python/legion_linux
+#set -ex
+#DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#cd ${DIR}/../python/legion_linux
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root to install"
-  exit
-else
-    xargs rm -rf < files.txt
-fi
+#if [ "$EUID" -ne 0 ]
+#  then echo "Please run as root to install"
+#  exit
+#else
+#    xargs rm -rf < files.txt
+#fi
