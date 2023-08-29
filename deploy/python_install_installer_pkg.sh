a3e059cf7591ab2ab7c3dd732009c54844fdbdec
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-08-29 22:17:22 +0100

    update scripts to use TAG
    The TAG vatiable will only use the number and '.' it will ignore text and other caracters
    EX:0.0.1-prerelease = 0.0.1
    Pls dont use version like: 0.0.1-1 = 0.0.11

--- deploy/python_install_installer_pkg.sh
+++ deploy/python_install_installer_pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 REPODIR="${DIR}/.."
-TAG="1.0.0"
+TAG=$(git describe --abbrev=0 | sed 's/[^0-9.]*//g')
 
 cd ${REPODIR}/python/legion_linux
 sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
@@ -11,13 +11,13 @@ sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
 # Build and install python package with build and installer
 python3 -m build --wheel --no-isolation
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root to install"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root to install"
+	exit
 else
-    python3 -m installer --destdir="/" dist/*.whl
-    #Create config folder
-    cp -r /usr/share/legion_linux /etc/legion_linux
+	python3 -m installer --destdir="/" dist/*.whl
+	#Create config folder
+	cp -r /usr/share/legion_linux /etc/legion_linux
 fi
 # sudo cp legion_linux/legion_gui.desktop /usr/share/applications/
 # sudo cp legion_linux/legion_gui_user.desktop /usr/share/applications/
--- deploy/python_install_pip_pkg.sh
+++ deploy/python_install_pip_pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 REPODIR="${DIR}/.."
-TAG="1.0.0"
+TAG=$(git describe --abbrev=0 | sed 's/[^0-9.]*//g')
 
 cd ${REPODIR}/python/legion_linux
 sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
@@ -11,14 +11,14 @@ sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
 python3 -m pip install --upgrade build installer
 python3 -m build
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root to install"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root to install"
+	exit
 else
-    python3 -m installer --destdir="/" dist/*.whl
-    #Create config folder
-    #cp -r /usr/share/legion_linux /etc/legion_linux
-    cp -r /usr/local/share/legion_linux /etc/legion_linux
+	python3 -m installer --destdir="/" dist/*.whl
+	#Create config folder
+	#cp -r /usr/share/legion_linux /etc/legion_linux
+	cp -r /usr/local/share/legion_linux /etc/legion_linux
 fi
 # Desktop file
 # sudo cp legion_linux/legion_gui.desktop /usr/share/applications/
