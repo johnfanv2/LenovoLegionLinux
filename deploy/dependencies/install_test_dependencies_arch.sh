b4095e035efb0b03322abb0cf4fa67d23250e167
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-08-30 22:52:56 +0100

    fix arch depedencies dkms package

--- deploy/dependencies/install_test_dependencies_arch.sh
+++ deploy/dependencies/install_test_dependencies_arch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ex
-sudo pacman -S --noconfirm linux-headers git xorg-server-xvfb libxcb base-devel
+sudo pacman -S --noconfirm linux-headers git xorg-server-xvfb libxcb base-devel dkms i2c-tools dmidecode fakeroot
