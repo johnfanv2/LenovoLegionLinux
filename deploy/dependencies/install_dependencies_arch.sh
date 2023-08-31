c261b17c866a3f9cbf7deefdf6805510a29c232c
johnfan <johndoe@example.com>
2023-08-31 20:31:55 +0200

    Disable download timeout for Arch container tests

--- deploy/dependencies/install_dependencies_arch.sh
+++ deploy/dependencies/install_dependencies_arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-sudo pacman -S --noconfirm linux-headers base-devel lm_sensors git dmidecode python-pyqt5 python-yaml python-argcomplete polkit python-build python-installer python-wheel python-setuptools
-sudo pacman -S --noconfirm dkms openssl mokutil
+sudo pacman -S --disable-download-timeout --noconfirm linux-headers base-devel lm_sensors git dmidecode python-pyqt5 python-yaml python-argcomplete polkit python-build python-installer python-wheel python-setuptools
+sudo pacman -S --disable-download-timeout --noconfirm dkms openssl mokutil
