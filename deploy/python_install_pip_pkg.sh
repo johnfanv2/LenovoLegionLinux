eb7a3a374ffa690a29f940490cff0521caf01bec
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-09-24 18:40:18 +0000

    Update python install deploy scripts

--- deploy/python_install_pip_pkg.sh
+++ deploy/python_install_pip_pkg.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+#!/bin/bash
 set -ex
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 REPODIR="${DIR}/.."
-TAG=$(git describe --tags --abbrev=0 | sed 's/[^0-9.]*//g')
 
-cd ${REPODIR}/python/legion_linux
+#Install python darkdetect
+cp --recursive ${REPODIR}/deploy/build_packages/{setup.cfg,setup.py} ${REPODIR}/subprojects/darkdetect
+cd ${REPODIR}/subprojects/darkdetect
+
+TAG=$(git describe --tags --abbrev=0 | sed 's/[^0-9.]*//g')
 sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
 
-# Build and install python package
 python3 -m pip install --upgrade build installer
 python3 -m build
 
@@ -16,14 +19,24 @@ if [ "$EUID" -ne 0 ]; then
 	exit
 else
 	python3 -m installer --destdir="/" dist/*.whl
-	#Create config folder
-	#cp -r /usr/share/legion_linux /etc/legion_linux
-	cp -r /usr/local/share/legion_linux /etc/legion_linux
 fi
-# Desktop file
-# sudo cp legion_linux/legion_gui.desktop /usr/share/applications/
-# sudo cp legion_linux/legion_gui_user.desktop /usr/share/applications/
-# sudo mkdir -p /usr/share/icons/
-# sudo cp legion_linux/legion_logo.png /usr/share/icons/legion_logo.png
+
+#Install LenovoLegionLinux python package
+cd ${REPODIR}
+TAG=$(git describe --tags --abbrev=0 | sed 's/[^0-9.]*//g')
+
+cd ${REPODIR}/python/legion_linux
+sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
+
+python3 -m build --wheel --no-isolation
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root to install"
+	exit
+else
+	python3 -m installer --destdir="/" dist/*.whl
+	#Create config folder (not overwrite old folder)
+	#cp -r /usr/share/legion_linux /etc/legion_linux
+fi
 
 echo "Done"
