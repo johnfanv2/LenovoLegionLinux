9ca19bc423fc7b9c8b1560c1f271aa5340202b1e
Gonçalo Negrier Duarte <68701049+MrDuartePT@users.noreply.github.com>
2023-12-04 19:11:06 +0000

    Update download_install_rpm_ppa.sh

--- deploy/package_testing/download_install_rpm_ppa.sh
+++ deploy/package_testing/download_install_rpm_ppa.sh
@@ -2,4 +2,4 @@
 sudo curl -s https://MrDuartePT.github.io/LLL-pkg-repo/fedora/LLL.repo | sudo tee /etc/yum.repos.d/LLL.repo > /dev/null
 sudo dnf config-manager --add-repo /etc/yum.repos.d/LLL.repo
 sudo dnf config-manager --set-enabled LLL-pkg-repo
-sudo dnf install -y dkms-lenovolegionlinux python3-lenovolegionlinux
+sudo dnf install -y dkms-lenovolegionlinux python-lenovolegionlinux
