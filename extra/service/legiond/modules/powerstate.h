b30e707c3016a41a27af90af9200130915f0aee6
ston <ston.jia@outlook.com>
2024-04-05 17:23:28 +0800

    legiond: use enum for powerstate

--- extra/service/legiond/modules/powerstate.h
+++ extra/service/legiond/modules/powerstate.h
@@ -1,13 +1,15 @@
 #ifndef POWERSTATE_H_
 #define POWERSTATE_H_
 
-#define P_AC_Q 0
-#define P_BAT_Q 1
-#define P_AC_B 2
-#define P_BAT_B 3
-#define P_AC_BP 4
-#define P_BAT_BP 5
-#define P_AC_P 6
+typedef enum _POWER_STATE {
+	P_AC_Q,
+	P_BAT_Q,
+	P_AC_B,
+	P_BAT_B,
+	P_AC_BP,
+	P_BAT_BP,
+	P_AC_P
+} POWER_STATE;
 
 #define P_ERROR_PROFILE -1
 #define P_ERROR_AC -2
@@ -15,6 +17,6 @@
 #define ac_path "/sys/class/power_supply/ADP0/online"
 #define profile_path "/sys/firmware/acpi/platform_profile"
 
-extern int get_powerstate();
+extern POWER_STATE get_powerstate();
 
 #endif // POWERSTATE_H_
--- extra/service/legiond/modules/setapply.h
+++ extra/service/legiond/modules/setapply.h
@@ -1,10 +1,11 @@
 #ifndef SETAPPLY_H_
 #define SETAPPLY_H_
 #include "parseconf.h"
+#include "powerstate.h"
 
-extern int set_fancurve(int power_state, LEGIOND_CONFIG *config);
-extern int set_cpu(int power_state, LEGIOND_CONFIG *config);
-extern int set_gpu(int power_state, LEGIOND_CONFIG *config);
-extern int set_all(int power_state, LEGIOND_CONFIG *config);
+extern int set_fancurve(POWER_STATE power_state, LEGIOND_CONFIG *config);
+extern int set_cpu(POWER_STATE power_state, LEGIOND_CONFIG *config);
+extern int set_gpu(POWER_STATE power_state, LEGIOND_CONFIG *config);
+extern int set_all(POWER_STATE power_state, LEGIOND_CONFIG *config);
 
 #endif // SETAPPLY_H_
