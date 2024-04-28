19033d5e0bd3dfcbe1b067194b77f179aaedcab6
ston <ston.jia@outlook.com>
2024-04-28 20:19:26 +0800

    kernel-driver: update NSCN
    disable acpi_check_dev
    
    Signed-off-by: ston <ston.jia@outlook.com>

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -675,7 +675,7 @@ static const struct model_config model_nscn = {
 	.access_method_temperature = ACCESS_METHOD_WMI3,
 	.access_method_fancurve = ACCESS_METHOD_WMI3,
 	.access_method_fanfullspeed = ACCESS_METHOD_WMI,
-	.acpi_check_dev = true,
+	.acpi_check_dev = false,
 	.ramio_physical_start = 0xFE0B0400,
 	.ramio_size = 0x600
 };
