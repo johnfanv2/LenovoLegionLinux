f33ea420fc51519cc9f861d78fa8ee71ab2da1b5
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-05-04 18:34:26 +0100

    deploy: not install darkdetect on test (is optional)
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- deploy/python_install_installer_pkg.sh
+++ deploy/python_install_installer_pkg.sh
@@ -3,21 +3,6 @@ set -ex
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 REPODIR="${DIR}/.."
 
-#Install python darkdetect
-cp --recursive ${REPODIR}/deploy/build_packages/{setup.cfg,setup.py} ${REPODIR}/subprojects/darkdetect
-cd ${REPODIR}/subprojects/darkdetect
-
-TAG=$(git describe --tags --abbrev=0 | sed 's/[^0-9.]*//g')
-sed -i "s/version = _VERSION/version = ${TAG}/g" setup.cfg
-python3 -m build --wheel --no-isolation
-
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run as root to install"
-	exit
-else
-	python3 -m installer --destdir="/" dist/*.whl
-fi
-
 #Install LenovoLegionLinux python package
 cd ${REPODIR}
 TAG=$(git describe --tags --abbrev=0 | sed 's/[^0-9.]*//g')
