a72a5f20603c018cc4c0bc81cb675f2799081a4f
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-05-04 18:15:57 +0100

    packages: drop Ubuntu/debian package build
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- deploy/package_testing/download_install_deb_ppa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y curl gpg
-sudo curl -s https://MrDuartePT.github.io/LLL-pkg-repo/ubuntu/KEY.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lll-ppa.gpg > /dev/null
-sudo curl -SsL -o /etc/apt/sources.list.d/lll-ppa.list https://MrDuartePT.github.io/LLL-pkg-repo/ubuntu/lll-ppa.list
-sudo apt update
-sudo apt install -y lenovolegionlinux-dkms python3-legion-linux
---
+++ deploy/package_testing/download_install_debian.sh
@@ -0,0 +1,8 @@
+sudo curl https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
+sudo curl https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/python3-legion-linux_0.0.10+ds-2_all.deb -o /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
+sudo curl https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/legiond_0.0.10+ds-2_amd64.deb
+
+sudo apt install /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
+sudo apt install /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
+sudo apt install /tmp/legiond_0.0.10+ds-2_amd64.deb
+
