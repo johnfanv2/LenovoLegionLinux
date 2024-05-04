1d870ade8b16f019347e45f01ddcfe0006a28eb3
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-05-04 19:42:28 +0100

    deploy: add -y to apt
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- deploy/package_testing/download_install_debian.sh
+++ deploy/package_testing/download_install_debian.sh
@@ -1,10 +1,10 @@
-sudo apt install curl
+sudo apt install -y curl
 
 sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
 sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/python3-legion-linux_0.0.10+ds-2_all.deb -o /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
 sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/legiond_0.0.10+ds-2_amd64.deb
 
-sudo apt install /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
-sudo apt install /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
-sudo apt install /tmp/legiond_0.0.10+ds-2_amd64.deb
+sudo apt install -y /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
+sudo apt install -y /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
+sudo apt install -y /tmp/legiond_0.0.10+ds-2_amd64.deb
 
