c159e5c4972d0ef6e75fdc581eb89df693ed94bf
Biaogo <481622717@qq.com>
2025-03-27 13:58:47 +0800

    Modify code

--- python/legion_linux/legion_linux/legion.py
+++ python/legion_linux/legion_linux/legion.py
@@ -1540,12 +1540,7 @@ class LegionModelFacade:
         log.info("Boot logo has been enabled successfully in EFIVars.")
         full_logo_dir = os.path.join(self.boot_dir, self.logo_dir.lstrip("/"))
         if os.path.exists(full_logo_dir):
-            for filename in os.listdir(full_logo_dir):
-                file_path = os.path.join(full_logo_dir, filename)
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
+            shutil.rmtree(full_logo_dir)
         os.makedirs(full_logo_dir, exist_ok=True)
         dest_filename = f"mylogo_{img_w}x{img_h}.{img_fmt}"
         dest_path = os.path.join(full_logo_dir, dest_filename)
