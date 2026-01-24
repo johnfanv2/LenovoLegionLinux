974d574e6267cbe9979f313e65b5650ed8258170
qquique <179249318+qquique@users.noreply.github.com>
2026-01-24 14:42:36 +0900

    fix CI containers tests

--- deploy/dependencies/install_test_dependencies_arch.sh
+++ deploy/dependencies/install_test_dependencies_arch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -ex
-sudo pacman -S --noconfirm --disable-download-timeout linux-headers git xorg-server-xvfb libxcb base-devel dkms i2c-tools dmidecode fakeroot
+sudo pacman -S --noconfirm --disable-download-timeout linux-headers git xorg-server-xvfb libxcb base-devel dkms i2c-tools dmidecode fakeroot python-pillow
+
--- deploy/package_testing/download_install_debian.sh
+++ deploy/package_testing/download_install_debian.sh
@@ -1,10 +1,9 @@
 sudo apt install -y curl
 
-sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
-sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/python3-legion-linux_0.0.10+ds-2_all.deb -o /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
-sudo curl --insecure https://ftp.de.debian.org/debian/pool/contrib/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb -o /tmp/legiond_0.0.10+ds-2_amd64.deb
+sudo curl --insecure https://ftp.de.debian.org/debian/pool/main/l/lenovolegionlinux/lenovolegionlinux-dkms_0.0.20+ds-1.1_amd64.deb -o /tmp/lenovolegionlinux-dkms_0.0.20+ds-1.1_amd64.deb
+sudo curl --insecure https://ftp.de.debian.org/debian/pool/main/l/lenovolegionlinux/python3-legion-linux_0.0.20+ds-1.1_all.deb -o /tmp/python3-legion-linux_0.0.20+ds-1.1_all.deb
+sudo curl --insecure https://ftp.de.debian.org/debian/pool/main/l/lenovolegionlinux/legiond_0.0.20+ds-1.1_amd64.deb -o /tmp/legiond_0.0.20+ds-1.1_amd64.deb
 
-sudo apt install -y /tmp/lenovolegionlinux-dkms_0.0.10+ds-2_amd64.deb
-sudo apt install -y /tmp/python3-legion-linux_0.0.10+ds-2_all.deb
-sudo apt install -y /tmp/legiond_0.0.10+ds-2_amd64.deb
-
+sudo apt install -y /tmp/lenovolegionlinux-dkms_0.0.20+ds-1.1_amd64.deb
+sudo apt install -y /tmp/python3-legion-linux_0.0.20+ds-1.1_all.deb
+sudo apt install -y /tmp/legiond_0.0.20+ds-1.1_amd64.deb
