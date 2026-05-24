34542f2eb0519eb131a6a35c5c3d83561eea5d10
09277632 <103803785+09277632@users.noreply.github.com>
2026-05-24 09:34:59 +0800

    Update legion-laptop.c
    Remove outdated configurations and add fast charging control paths for model_J2CN.

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -499,6 +499,10 @@ static const struct model_config model_j2cn = {
 	.acpi_check_dev = true,
 	.ramio_physical_start = 0xFE00D400,
 	.ramio_size = 0x600
+	.acpi_paths = {
+		[ACPI_PATH_READ_RAPIDCHARGE] = "\\_SB.PC00.LPCB.EC0.VPC0.GBMD",
+		[ACPI_PATH_WRITE_RAPIDCHARGE] = "\\_SB.PC00.LPCB.EC0.VPC0.SBMC"
+	}
 };
 
 static const struct model_config model_9vcn = {
@@ -1188,15 +1192,6 @@ static const struct dmi_system_id optimistic_allowlist[] = {
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
 			DMI_MATCH(DMI_BIOS_VERSION, "H1CN"),
-		},
-		.driver_data = (void *)&model_v0
-	},
-	{
-		// Release year: 2022
-		.ident = "J2CN",
-		.matches = {
-			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
-			DMI_MATCH(DMI_BIOS_VERSION, "J2CN"),
 		},
 		.driver_data = (void *)&model_v0
 	},
