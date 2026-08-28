f2eb7e09234840c2bfe05420cb6e2421c0d5c930
LolPopGames <157955292+LolPopGames@users.noreply.github.com>
2026-08-28 20:36:17 +0300

    feat: add support for LOQ 15AHP10 (R8CN)

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -1313,6 +1313,32 @@ static const struct model_config model_nzcn = {
 	.has_fancurve_defaults = true
 };
 
+// LOQ 15AHP10
+static const struct model_config model_r8cn = {
+    .registers = &ec_register_offsets_loq_v1,
+    .check_embedded_controller_id = true,
+    .embedded_controller_id = 0x5508,
+    .memoryio_physical_ec_start = 0xC400,
+    .memoryio_size = 0x300,
+    .has_minifancurve = true,
+    .has_custom_powermode = true,
+    .has_extreme_powermode = true,
+    .access_method_powermode = ACCESS_METHOD_WMI,
+    .access_method_keyboard = ACCESS_METHOD_WMI2,
+    .access_method_fanspeed = ACCESS_METHOD_WMI3,
+    .access_method_temperature = ACCESS_METHOD_WMI3,
+    .access_method_fancurve = ACCESS_METHOD_EC3,
+    .access_method_fanfullspeed = ACCESS_METHOD_WMI3,
+    .acpi_check_dev = false,
+    .ramio_physical_start = 0xFE0B0F00,
+    .ramio_size = 0x600,
+    .acpi_paths = {
+        [ACPI_PATH_STA] = "\\_SB.PC00.LPCB.EC0.VPC0._STA",
+        [ACPI_PATH_CFG] = "\\_SB.PC00.LPCB.EC0.VPC0._CFG"
+    },
+    .has_fancurve_defaults = true
+};
+
 // Legion Slim 5 16AHP9 (2024) - Model 83DH
 static const struct model_config model_nrcn = {
 	.registers = &ec_register_offsets_v0,
@@ -1904,6 +1930,15 @@ static const struct dmi_system_id optimistic_allowlist[] = {
 			DMI_MATCH(DMI_BIOS_VERSION, "NZCN"),
 		},
 		.driver_data = (void *)&model_nzcn
+	},
+	{
+		// e.g. LOQ 15AHP10 (AMD Ryzen 7 250 + RTX 5050)
+		.ident = "R8CN",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_BIOS_VERSION, "R8CN"),
+		},
+		.driver_data = (void *)&model_r8cn
 	},
 	{
 		// e.g. Legion 5 15IRX10 (83LY)
