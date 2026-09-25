c895d103b4ff010d3057f6daa06d2efb3075b594
09277632 <103803785+09277632@users.noreply.github.com>
2026-09-26 00:11:18 +0800

    Update legion-laptop.c

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -618,6 +618,8 @@ static const struct model_config model_j2cn = {
 	.ramio_physical_start = 0xFE00D400,
 	.ramio_size = 0x600,
 	.acpi_paths = {
+		[ACPI_PATH_STA] = "\\_SB.PC00.LPCB.EC0.VPC0._STA",
+		[ACPI_PATH_CFG] = "\\_SB.PC00.LPCB.EC0.VPC0._CFG"，
 		[ACPI_PATH_READ_RAPIDCHARGE] = "\\_SB.PC00.LPCB.EC0.VPC0.GBMD",
 		[ACPI_PATH_WRITE_RAPIDCHARGE] = "\\_SB.PC00.LPCB.EC0.VPC0.SBMC"
 	}
